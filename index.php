e3287e3768f03df9b5bb90df3240905ce1c5eb88
Daniel Heredia <danielhmetro@gmail.com>
2023-04-30 22:25:08 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="data:,">
+<style>
+html {
+font-family: Helvetica;
+display: inline-block;
+margin: 0px auto;
+text-align: center;
+}
+.button {
+background-color: #4CAF50;
+border: none;
+color: white;
+width: 200px;
+height: 200px;
+text-decoration: none;
+font-size: 30px;
+margin: 2px;
+cursor: pointer;
+}
+</style>
+</head>
+
+<body>
+<h1>Door</h1>
+<br><p>Press the button to open the door!</p><br>
+<p><a href="/open.php"><button class="button">OPEN</button></a></p>
+
+</body>
+</html>
